ae04f7eec0347fcdab40e6b162e7b82728f81c24
Dmitri <unabl4@gmail.com>
2018-03-17 21:21:38 +0200

    slightly improve the solution comment for the 'evil valentine' challenge solution (Ruby)

--- evil_valentine/evil_valentine.rb
+++ evil_valentine/evil_valentine.rb
@@ -1,8 +1,8 @@
 # https://codefights.com/challenge/S7D6TQsx8bvhdqyau
 
 # Solution: the main idea is to find the angle between the tower and horizon.
-# Knowing the angle and the radius will allow to calculate the length of the arc
-# or the the "distance" from the tower to the flower
+# Knowing the angle and the radius of the sphere will allow to calculate the
+# length of the arc or the the "distance" from the tower to the flower.
 # Basic trigonometry is enough.
 
 def evilValentine h,r
