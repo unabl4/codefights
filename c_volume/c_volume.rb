eb3828875f443e0e4bb4ceef5fdaac8063614068
Dmitri <unabl4@gmail.com>
2018-03-22 22:13:49 +0200

    'cVolume' challenge solution (Ruby)

---
+++ c_volume/c_volume.rb
@@ -0,0 +1,8 @@
+# solution: the main idea is to subtract the area of the triangle (second term)
+# from the area of the sector (first term)
+
+def cVolume r,l,m
+  return 0 if r == 0
+  z = r-m
+  l*(r*r * Math.acos(z/r)-z*(2*r*m - m*m)**0.5)
+end
