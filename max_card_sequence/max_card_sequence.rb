121524184f81d990eabd0fdd6df45cbfdf516da2
Dmitri <unabl4@gmail.com>
2018-05-04 22:11:01 +0300

    'maxCardSequence' challenge solution (Ruby)

---
+++ max_card_sequence/max_card_sequence.rb
@@ -0,0 +1,19 @@
+# https://codefights.com/challenge/kjXSxYfm4ZfvdzAs4/solutions
+
+def maxCardSequence(cards)
+    cards.sort! # sort
+    
+    l = 1
+    s = cards[0] % 2
+    n = cards.size # length
+    (1...n).each do |idx|
+        y = cards[idx] % 2
+        if y != s
+            s = y # memo
+            l += 1
+        end
+    end
+    
+    l
+end
+
