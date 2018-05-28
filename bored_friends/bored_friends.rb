b6d662fa4c97c1151a22f99b5ab2fe00a1c139e6
Dmitri <unabl4@gmail.com>
2018-05-28 10:03:31 +0300

    'boredFriends' challenge solution (Ruby)

---
+++ bored_friends/bored_friends.rb
@@ -0,0 +1,9 @@
+# https://codefights.com/challenge/X2mPWE5pr4okydBF4
+
+# N:1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18 ...
+# W:D|D|D|D|D|D|D|D|T| D| D| D| D| D| D| D| D| T ...
+
+def boredFriends(n)
+    n.to_i % 9
+end
+
