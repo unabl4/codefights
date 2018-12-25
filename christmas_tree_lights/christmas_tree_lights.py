82c4e0e2c68ac50e2715d4802fe6069210ede94b
Dmitri <unabl4@gmail.com>
2018-12-25 20:06:35 +0200

    'christmasTreeLights' challenge solution (Python 3)

---
+++ christmas_tree_lights/christmas_tree_lights.py
@@ -0,0 +1,6 @@
+# https://app.codesignal.com/challenge/QipxbbavMtKppH4KW
+
+# hi, Anastasiya_8; Long time no see :D
+def christmasTreeLights(n):
+    # sum of odd numbers = 1+3+5+7+...
+    return n**2
