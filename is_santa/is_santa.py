c33438f902487134f4a5bfc94c7ba521baeec331
Dmitri <unabl4@gmail.com>
2018-12-26 15:29:00 +0200

    'isSanta' challenge solution (Python 3)

---
+++ is_santa/is_santa.py
@@ -0,0 +1,4 @@
+# https://app.codesignal.com/challenge/6WWYrtRghnkyiJGej
+
+def isSanta(s):
+    return "It is Santa! Merry Christmas and a Happy New Year!" if "hohoho" in s.lower() else "Hey, someone! Merry Christmas and have a nice day!"
